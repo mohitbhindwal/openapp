2a11efe3bb6dcce96f26ebab2fbf132dc6ae44a3
unknown <bhindwal.mohit@gmail.com>
2018-03-10 14:03:24 +0530

    Update java

--- src/main/java/com/mohit/openapp/MTest.java
+++ src/main/java/com/mohit/openapp/MTest.java
@@ -1,10 +1,71 @@
 package com.mohit.openapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
 public class MTest {
+	
+	
+	static {
+ 
+		
+		System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
 
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+		 
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/mohitdb", "mohitdb",
+					"mohitdb");
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+		 
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	
 	}
 
-}
+	public static void main(String[] args) {}
+
+		
+		
+	}
+
+	 
+
